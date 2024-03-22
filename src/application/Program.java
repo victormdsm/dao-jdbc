ac71f487b3922260f5dd1b990a5ee3d23d662d32
victormdsm <victor.de.souza.muller@gmail.com>
2024-03-22 17:56:44 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,10 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.sql.Connection;
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
@@ -11,5 +13,8 @@ public class Program {
         Department obj = new Department(1, "books");
         System.out.println(obj);
 
+        Seller seller = new Seller(1, "joao", "joao@hotmail.com", new Date(), 3000.0, obj);
+
+        System.out.println(seller);
     }
 }
