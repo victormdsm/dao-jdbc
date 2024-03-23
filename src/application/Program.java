c8ca5af2341cf6d717c02eddc78db144c426fdcd
victormdsm <victor.de.souza.muller@gmail.com>
2024-03-23 14:50:38 -0300

    Find by id

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,10 +12,9 @@ public class Program {
     public static void main(String[] args) {
         Connection conn = null;
 
-        Department obj = new Department(1, "books");
-        System.out.println(obj);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        Seller seller = new Seller(1, "joao", "joao@hotmail.com", new Date(), 3000.0, obj);
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
     }
