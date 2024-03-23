06ebacf3afbebf963a1a9d08e71595fd66574a47
victormdsm <victor.de.souza.muller@gmail.com>
2024-03-23 14:12:53 -0300

    sellerdaojdbc

---
+++ src/model/impl/SellerDaoJDBC.java
@@ -0,0 +1,35 @@
+package model.impl;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao {
+
+
+    @Override
+    public void insert(Seller obj) {
+
+    }
+
+    @Override
+    public void update(Seller obj) {
+
+    }
+
+    @Override
+    public void deletById(Integer id) {
+
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return null;
+    }
+}
