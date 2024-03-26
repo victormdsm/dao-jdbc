07f1f8921f50e1bf74b6ed0cf4fa0f2ddc790869
victormdsm <victor.de.souza.muller@gmail.com>
2024-03-26 16:24:04 -0300

    finish project

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -6,6 +6,7 @@ import model.entities.Department;
 import model.impl.DepartmentDaoJDBC;
 
 import java.sql.Connection;
+import java.util.List;
 
 public class Program2 {
     public static void main(String[] args) {
@@ -16,6 +17,26 @@ public class Program2 {
         obj.setName("Mamonha");
         dep.insert(obj);
         System.out.println(obj.toString());
+
+        System.out.println("Teste Update");
+        Department obj2 = new Department(2, "Mamonhoite");
+        dep.update(obj2);
+        System.out.println(obj2);
+
+        System.out.println("Teste delete");
+        dep.deletById(5);
+
+        System.out.println("Teste find id");
+        Department depFind = dep.findById(3);
+        System.out.println(depFind);
+
+        System.out.println("Teste find all");
+        List<Department> departments = dep.findAll();
+        for (Department deps: departments)   {
+            System.out.println(deps);
+        }
+
+
 
     }
 }
--- src/model/impl/DepartmentDaoJDBC.java
+++ src/model/impl/DepartmentDaoJDBC.java
@@ -1,9 +1,12 @@
 package model.impl;
 
+import db.DB;
+import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DepartmentDaoJDBC implements DepartmentDao {
@@ -30,26 +33,91 @@ public class DepartmentDaoJDBC implements DepartmentDao {
             }
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            DB.closeStatement(ps);
         }
     }
 
     @Override
     public void update(Department obj) {
+        PreparedStatement ps = null;
+
+        try {
+            ps = conn.prepareStatement("UPDATE department SET name=? WHERE id=?");
+            ps.setString(1, obj.getName());
+            ps.setInt(2, obj.getId());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DB.closeStatement(ps);
+        }
 
     }
 
     @Override
     public void deletById(Integer id) {
+        PreparedStatement ps = null;
 
+        try {
+            ps = conn.prepareStatement("DELETE FROM department WHERE id=?");
+            ps.setInt(1, id);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(ps);
+        }
     }
 
     @Override
     public Department findById(Integer id) {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            ps = conn.prepareStatement("SELECT * FROM department WHERE id=?");
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            if(rs.next()) {
+                return instantiateDepartment(rs);
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+            DB.closeStatement(ps);
+            DB.closeResultSet(rs);
+        }
         return null;
+    }
+
+    public Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department dep = new Department(rs.getInt("id"), rs.getString("Name"));
+        return dep;
     }
 
     @Override
     public List<Department> findAll() {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            ps = conn.prepareStatement("SELECT * FROM department");
+            rs = ps.executeQuery();
+            List<Department> departments = new ArrayList<>();
+            while (rs.next()) {
+                Department dep = instantiateDepartment(rs);
+                departments.add(dep);
+            }
+            return departments;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            DB.closeStatement(ps);
+            DB.closeResultSet(rs);
+        }
         return null;
     }
 }
